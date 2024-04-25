2556cb612473ab3c4b5f8173eae2020e22dbe0e4
J4V3 <133072334+tonyrose02@users.noreply.github.com>
2024-04-24 23:11:49 -0600

    .

--- app/demo/page.jsx
+++ app/demo/page.jsx
@@ -1,23 +1,23 @@
-import { ToDoList } from '@/components/todos/ToDoList';
-import { AddTask } from '@/components/dialogs/AddTask';
-import { ToDoListItem } from '@/components/todos/ToDoListItem';
+// import { ToDoList } from '@/components/todos/ToDoList';
+// import { AddTask } from '@/components/dialogs/AddTask';
+// import { ToDoListItem } from '@/components/todos/ToDoListItem';
 
-import { getToDoItems } from '@/lib/firebase/api';
+// import { getToDoItems } from '@/lib/firebase/api';
 
-async function DemoPage({ children }) {
-    const payload = await getToDoItems();
-    const tasks = Object.entries(payload.todos);
+// async function DemoPage({ children }) {
+//     const payload = await getToDoItems();
+//     const tasks = Object.entries(payload.todos);
 
-    return (
-        <>
-            <header className="text-center pt-28">
-                <h2 className="text-5xl text-slate-900">Demo Page Component</h2>
-            </header>
-            <main className="max-w-md mx-auto py-24">
-                <AddTask />
-            </main>
-        </>
-    );
-}
+//     return (
+//         <>
+//             <header className="text-center pt-28">
+//                 <h2 className="text-5xl text-slate-900">Demo Page Component</h2>
+//             </header>
+//             <main className="max-w-md mx-auto py-24">
+//                 <AddTask />
+//             </main>
+//         </>
+//     );
+// }
 
-export default DemoPage;
+// export default DemoPage;
